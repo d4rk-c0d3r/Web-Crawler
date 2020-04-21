a6d5a6cba0b3130457af08075b8e252629096cac
d4rk-c0d3r <64041468+d4rk-c0d3r@users.noreply.github.com>
2020-04-21 09:39:36 -0500

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -8,9 +8,12 @@ def request(url):
 
 target_url = "rvce.edu.in"
 with open("/home/user/Downloads/files-and-dirs-wordlist.txt","r") as wordlist_file:
-    for line in wordlist_file:
-        word = line.strip()
-        test_url = target_url + "/" + word
-        response = request(test_url)
-        if response:
-            print("[+] Discovered subdomain >> " + test_url)
+    try:
+        for line in wordlist_file:
+            word = line.strip()
+            test_url = target_url + "/" + word
+            response = request(test_url)
+            if response:
+                print("[+] Discovered subdomain >> " + test_url)
+    except KeyboardInterrupt:
+        exit()
